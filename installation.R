78e3aa541cac813575fa9bf3badc5c13165ab761
Pearl <linao264590000@gmail.com>
2017-11-24 18:16:09 +0800

    Add files via upload

---
+++ installation.R
@@ -0,0 +1,29 @@
+
+# 先安裝好R以及Rstudio
+# 開啟R
+# install `swirl R package`
+source("http://wush978.github.io/R/init-swirl.R")
+
+
+
+# 如果出現以下訊息
+# ERROR: dependency 'stringi' is not available for package 'swirl'
+
+# `dependecies = TRUE` 會順便下載`stringi`
+install.packages("swirl", dependencies = TRUE)
+
+
+
+# 就可以開始引入swirl做你想做的事情
+library(swirl)
+swirl()
+
+
+
+# 若想要上別的課程呢？
+# 先 `library(swirl)`
+# 基本課程
+install_course_url("https://wush978.github.io/R/course/DataScienceAndR.zip")
+# Others
+install_course("R Programming")
+install_course("Exploratory Data Analysis")
